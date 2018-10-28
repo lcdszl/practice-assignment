d292ef1a9de7597126ce5b14ac22bb8682eea432
lcdszl <34284944+lcdszl@users.noreply.github.com>
2018-10-28 18:55:44 -0400

    untracked Library, added wrappe manager for player and enemy

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public GameObject instance;
+    [HideInInspector]
+    public int enemyNum;
+
+    private EnemyMovement enemyMovement;
+    private FieldOfView fieldOfView;
+
+    public void Setup()
+    {
+        enemyMovement = instance.GetComponent<EnemyMovement>();
+        fieldOfView = instance.GetComponent<FieldOfView>();
+    }
+
+    public void DisableMovement()
+    {
+        enemyMovement.enabled = false;
+        fieldOfView.enabled = false;
+    }
+
+    public void EnableMovement()
+    {
+        enemyMovement.enabled = true;
+        fieldOfView.enabled = true;
+    }
+
+    public void Reset()
+    {
+        instance.transform.position = spawnPoint.position;
+
+        instance.SetActive(false);
+        instance.SetActive(true);
+    }
+
+}
