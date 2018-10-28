70987483b23a587cf7fbf189534403c1989a2d3c
lcdszl <34284944+lcdszl@users.noreply.github.com>
2018-10-28 18:57:54 -0400

    adding player manager wrapper

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,8 +5,40 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour {
     [HideInInspector]
     public Color playerColor;
+    [HideInInspector]
+    public int playerNum;
+    [HideInInspector]
+    public GameObject instance;
 
     public Transform spawnPoint;
 
+    private PlayerMovement playerMovement;
+    private PlayerContent playerContent;
+
+    public void Setup()
+    {
+        playerMovement = instance.GetComponent<PlayerMovement>();
+        playerContent = instance.GetComponent<PlayerContent>();
+    }
+
+    public void DisableControl()
+    {
+        playerMovement.enabled = false;
+        playerContent.enabled = false;
+    }
+
+    public void EnableControl()
+    {
+        playerMovement.enabled = true;
+        playerContent.enabled = true;
+    }
+
+    public void Reset()
+    {
+        instance.transform.position = spawnPoint.position;
+
+        instance.SetActive(false);
+        instance.SetActive(true);
+    }
 
 }
