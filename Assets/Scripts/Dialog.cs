8e80702fb1b59e4d2691a6a15a2c2b63bd11767d
lcdszl <34284944+lcdszl@users.noreply.github.com>
2018-11-07 21:54:32 -0500

    dialog in progress

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog {
+
+    public string[] Dialogs;
+    public string Name;
+
+}
---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+
+    private Queue<string> sentences;
+
+    public DialogManager()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialog dialogToDisplay)
+    {
+        sentences.Clear();
+        Debug.Log("start dialog");
+        foreach (string sentence in dialogToDisplay.Dialogs)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextDialog();
+    }
+
+    public void DisplayNextDialog()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+        Debug.Log(sentences.Dequeue());
+    }
+
+    public void EndDialog()
+    {
+        Debug.Log("end dialog");
+    }
+
+}
