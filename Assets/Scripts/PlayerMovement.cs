13a46b92832c931877f4d24d2a0785b2ca7b6110
Tony <liyuxuan6699@gmail.com>
2018-11-19 15:34:07 -0500

    Player Movements on IOS

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,50 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     private Rigidbody2D rb2d;
     public float velocity = 10f;
-    //181106LIYUX
-    private Animator playerAnimation; 
-    //181106
-	// Use this for initialization
-	void OnEnable () {
+
+    private Vector2 touchPoint = -Vector2.one;
+
+    // Use this for initialization
+    void OnEnable()
+    {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
-        //181106LIYUX
-        playerAnimation = GetComponent<Animator>();
-        //181106
-	}
+    }
 
     private void OnDisable()
     {
         rb2d.velocity = new Vector2(0, 0);
     }
 
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
+
+#if UNITY_STANDALONE || UNITY_STANDALONE_OSX
 
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
 
-        Vector2 movement = new Vector2(h * Mathf.Abs(Mathf.Cos(Mathf.Atan2(v, h))), v * Mathf.Abs(Mathf.Sin(Mathf.Atan2(v, h))));
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_IPHONE
+        float v = 0;
+        float h = 0;
+        if (Input.touchCount > 0){
+            Touch myTouch = Input.touches[0];
+            touchPoint = myTouch.position;
+            v = touchPoint.y - (Screen.height / 2);
+            h = touchPoint.x - (Screen.width / 2);
+        }
+#endif
+
+
+        Vector2 movement = Vector3.Normalize(new Vector2(h * Mathf.Abs(Mathf.Cos(Mathf.Atan2(v, h))), v * Mathf.Abs(Mathf.Sin(Mathf.Atan2(v, h))))) ;
 
         rb2d.velocity = movement * velocity;
-        //181106LIYUX
-        playerAnimation.SetFloat("Speed",Mathf.Abs(rb2d.velocity.x));
-        if(h > 0){
-            transform.localScale = new Vector2(3f,3f);
-        }
-        else if(h < 0){
-            transform.localScale = new Vector2(-3f,3f);
-        }
+    }
 
-        //181106
-     
-	}
-
-
-   
 
 
 }
