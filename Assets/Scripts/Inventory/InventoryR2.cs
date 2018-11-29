f600808e1a886b3ddeba8b444a6cf0276efc5759
Tony <liyuxuan6699@gmail.com>
2018-11-28 22:41:23 -0500

    Inventory&Item onclick Trigger

---
+++ Assets/Scripts/Inventory/InventoryR2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryR2 : MonoBehaviour {
+
+   
+    public const int numItemSlots = 4;
+    public bool[] isFull = new bool[numItemSlots];
+    public GameObject[] itemSlots = new GameObject[numItemSlots];
+}
---
+++ Assets/Scripts/Inventory/Items/MagnetBtnTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetBtnTrigger : MonoBehaviour {
+    private Transform GamePlayer;
+    
+    // Use this for initialization
+    private void Start () {
+        GamePlayer = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+    public void Use(){
+
+        PlayerMovement playerMovement = GamePlayer.GetComponent<PlayerMovement>();
+        playerMovement.velocity = playerMovement.velocity * 2;
+        Destroy(gameObject);
+
+    }
+}
